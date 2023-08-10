2f929a4d4210a1052c12dc405ed3cdf8927308f8
lh <2731164534@qq.com>
2023-08-10 22:03:02 +0800

    feat: SwitchPro组件颜色样式修复

--- src/components/SwitchPro/index.tsx
+++ src/components/SwitchPro/index.tsx
@@ -18,8 +18,8 @@ export default ({ size = 1 }: Param) => {
         //@ts-ignore
         '--width': `${size * 44}px`, //按钮宽度
         '--height': `${size * 22}px`, //按钮高度
-        '--borderWidth':`${size * 2}px`, //label边框宽度
-        '--borderColor': isOn ? "#1677ff" : "rgba(0,0,0,0.45)",
+        '--borderWidth': `${size * 2}px`, //label边框宽度
+        '--borderColor': isOn ? '#1677ff' : '#888888',
         '--circleLeft': `${22 * size}px`, //圆形label到边缘距离
         '--translateX': isOn ? 0 : `${-22 * size}px`, //label移动距离
         '--textPadding': `${size * 8}px`, //文字距离边缘距离
